c00f9a571ba5472902c8af9adf3fed5fd561d93a
Matt Sullivan <mattsullivan@google.com>
2017-11-01 18:02:47 -0700

    Added hello world app

---
+++ 1_hello_world/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(
+      new Directionality(textDirection: TextDirection.ltr,
+        child: new Center(
+          child: new Text('Hello World!'))));
+}
