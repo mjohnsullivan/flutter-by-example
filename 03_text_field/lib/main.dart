991b1d564696e88345b6691ce12b9d5067770c37
Matt Sullivan <matt.j.sullivan@gmail.com>
2018-07-03 16:52:41 -0700

    Updated for Dart 2 and tweaked styling

--- 03_text_field/lib/main.dart
+++ 03_text_field/lib/main.dart
@@ -1,30 +1,37 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new TextFieldExampleApp());
+  runApp(TextFieldExampleApp());
 }
 
 class TextFieldExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Multi-Line Text Field Demo',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Multi-Line Text Field'),
+      home: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          title: const Text('Text Fields'),
         ),
-        body: new Column (
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            new SingleLineTextFieldWidget(),
-            new MultiLineTextFieldAndButtonWidget(), 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleLineTextFieldWidget(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MultiLineTextFieldAndButtonWidget(),
+            ),
           ],
         ),
       ),
@@ -35,14 +42,21 @@ class TextFieldExampleApp extends StatelessWidget {
 /// Single-line text field widget
 class SingleLineTextFieldWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext content) {
-    return new TextField(
-      autocorrect: false, // turns off auto-correct
-      decoration: new InputDecoration(
-        hintText: 'Enter text; return submits',
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
       ),
-      onChanged: (str) => print('Single-line text change: $str'),
-      onSubmitted: (str) => print('Single-line submitted: $str'),
+      child: TextField(
+        autocorrect: false, // turns off auto-correct
+        decoration: InputDecoration(
+          hintText: 'Enter text; return submits',
+        ),
+        onChanged: (str) => print('Single-line text change: $str'),
+        onSubmitted: (str) => _showInSnackBar(context, '$str'),
+      ),
     );
   }
 }
@@ -52,31 +66,52 @@ class MultiLineTextFieldAndButtonWidget extends StatefulWidget {
   MultiLineTextFieldAndButtonWidget({Key key}) : super(key: key);
 
   @override
-  _TextFieldAndButtonState createState() => new _TextFieldAndButtonState();
+  createState() => _TextFieldAndButtonState();
 }
 
-class _TextFieldAndButtonState extends State<MultiLineTextFieldAndButtonWidget> {
-  final TextEditingController _controller = new TextEditingController();
+class _TextFieldAndButtonState
+    extends State<MultiLineTextFieldAndButtonWidget> {
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new TextField(
-          controller: _controller,
-          maxLines: 10,
-          decoration: new InputDecoration(
-            hintText: 'Enter text; return doesn\'t submit',
+    return Container(
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+      //color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            maxLines: 10,
+            decoration: InputDecoration(
+              hintText: 'Enter text; return doesn\'t submit',
+            ),
+            onChanged: (str) => print('Multi-line text change: $str'),
+            onSubmitted: (str) =>
+                print('This will not get called when return is pressed'),
           ),
-          onChanged: (str) => print('Multi-line text change: $str'),
-          onSubmitted: (str) => print('This will not get called when return is pressed'),
-        ),
-        new RaisedButton(
-          onPressed: () => print('Multi-line submitted: ${_controller.text}'),
-          child: new Text('Submit'),
-        ),
-      ],
+          SizedBox(height: 10.0),
+          FlatButton(
+            onPressed: () => _showInSnackBar(
+                  context,
+                  '${_controller.text}',
+                ),
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
     );
   }
+}
+
+/// Displays text in a snackbar
+_showInSnackBar(BuildContext context, String text) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(text),
+      ));
 }
