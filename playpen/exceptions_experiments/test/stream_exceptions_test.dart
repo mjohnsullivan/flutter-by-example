3a7449b47e05ef6d109a3eea85354d3f78e3453f
Matt <matt.j.sullivan@gmail.com>
2018-08-13 03:44:48 +0000

    Added more tests

--- playpen/exceptions_experiments/test/stream_exceptions_test.dart
+++ playpen/exceptions_experiments/test/stream_exceptions_test.dart
@@ -139,4 +139,218 @@ void main() {
     await tester.pumpAndSettle(Duration(milliseconds: 10));
     expect(find.text('Exception: stream boom'), findsOneWidget);
   });
+
+  testWidgets('StreamBuilder handles future exceptions', (tester) async {
+    Future<int> futureExceptionThrower() async {
+      return await Future.delayed(Duration(microseconds: 1), () {
+        throw Exception('stream future boom');
+      });
+    }
+
+    Stream<int> streamFromFuture() {
+      final c = StreamController<int>();
+      futureExceptionThrower().then(
+        (i) => c.add(i),
+        onError: (e) => c.addError(e),
+      );
+      return c.stream;
+    }
+
+    await tester.pumpWidget(Builder(
+      builder: (context) => StreamBuilder(
+          stream: streamFromFuture(),
+          builder: (context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasError) {
+                return Text(
+                  '${snapshot.error}',
+                  textDirection: TextDirection.ltr,
+                );
+              }
+              return Text(
+                'Number is ${snapshot.data}',
+                textDirection: TextDirection.ltr,
+              );
+            }
+            return Text(
+              'waiting',
+              textDirection: TextDirection.ltr,
+            );
+          }),
+    ));
+
+    expect(find.text('waiting'), findsOneWidget);
+    await tester.pumpAndSettle(Duration(microseconds: 1));
+    expect(find.text('Exception: stream future boom'), findsOneWidget);
+  });
+
+  testWidgets('StreamBuilder handles future exceptions with async/await',
+      (tester) async {
+    Future<int> futureExceptionThrower() async {
+      return await Future.delayed(Duration(microseconds: 1), () {
+        throw Exception('stream future async boom');
+      });
+    }
+
+    void _addInt(StreamController<int> c) async {
+      try {
+        final i = await futureExceptionThrower();
+        c.add(i);
+      } catch (e) {
+        c.addError(e);
+      }
+    }
+
+    Stream<int> streamFromFuture() {
+      final c = StreamController<int>();
+      _addInt(c);
+      return c.stream;
+    }
+
+    await tester.pumpWidget(Builder(
+      builder: (context) => StreamBuilder(
+          stream: streamFromFuture(),
+          builder: (context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasError) {
+                return Text(
+                  '${snapshot.error}',
+                  textDirection: TextDirection.ltr,
+                );
+              }
+              return Text(
+                'Number is ${snapshot.data}',
+                textDirection: TextDirection.ltr,
+              );
+            }
+            return Text(
+              'waiting',
+              textDirection: TextDirection.ltr,
+            );
+          }),
+    ));
+
+    expect(find.text('waiting'), findsOneWidget);
+    await tester.pumpAndSettle(Duration(microseconds: 1));
+    expect(find.text('Exception: stream future async boom'), findsOneWidget);
+  });
+
+  testWidgets('StreamBuilder handles future exceptions with chained streams',
+      (tester) async {
+    Future<int> futureExceptionThrower() async {
+      return await Future.delayed(Duration(microseconds: 1), () {
+        throw Exception('stream future chained stream boom');
+      });
+    }
+
+    Stream<int> streamFromFuture() {
+      final c = StreamController<int>();
+      futureExceptionThrower().then(
+        (i) => c.add(i),
+        onError: (e) => c.addError(e),
+      );
+      return c.stream;
+    }
+
+    Stream<String> streamFromChained() {
+      final c = StreamController<String>();
+      final intStream = streamFromFuture();
+      intStream.listen(
+        (i) => c.add('$i'),
+        onError: (e) => c.addError(e),
+      );
+      return c.stream;
+    }
+
+    await tester.pumpWidget(Builder(
+      builder: (context) => StreamBuilder(
+          stream: streamFromChained(),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasError) {
+                return Text(
+                  '${snapshot.error}',
+                  textDirection: TextDirection.ltr,
+                );
+              }
+              return Text(
+                'Number is ${snapshot.data}',
+                textDirection: TextDirection.ltr,
+              );
+            }
+            return Text(
+              'waiting',
+              textDirection: TextDirection.ltr,
+            );
+          }),
+    ));
+
+    expect(find.text('waiting'), findsOneWidget);
+    await tester.pumpAndSettle(Duration(microseconds: 1));
+    expect(find.text('Exception: stream future chained stream boom'),
+        findsOneWidget);
+  });
+
+  testWidgets(
+      'StreamBuilder handles future exceptions with chained streams async/await',
+      (tester) async {
+    Future<int> futureExceptionThrower() async {
+      return await Future.delayed(Duration(microseconds: 1), () {
+        throw Exception('stream future chained stream async boom');
+      });
+    }
+
+    void _addInt(StreamController<int> c) async {
+      try {
+        final i = await futureExceptionThrower();
+        c.add(i);
+      } catch (e) {
+        c.addError(e);
+      }
+    }
+
+    Stream<int> streamFromFuture() {
+      final c = StreamController<int>();
+      _addInt(c);
+      return c.stream;
+    }
+
+    Stream<String> streamFromChained() {
+      final c = StreamController<String>();
+      final intStream = streamFromFuture();
+      intStream.listen(
+        (i) => c.add('$i'),
+        onError: (e) => c.addError(e),
+      );
+      return c.stream;
+    }
+
+    await tester.pumpWidget(Builder(
+      builder: (context) => StreamBuilder(
+          stream: streamFromChained(),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasError) {
+                return Text(
+                  '${snapshot.error}',
+                  textDirection: TextDirection.ltr,
+                );
+              }
+              return Text(
+                'Number is ${snapshot.data}',
+                textDirection: TextDirection.ltr,
+              );
+            }
+            return Text(
+              'waiting',
+              textDirection: TextDirection.ltr,
+            );
+          }),
+    ));
+
+    expect(find.text('waiting'), findsOneWidget);
+    await tester.pumpAndSettle(Duration(microseconds: 1));
+    expect(find.text('Exception: stream future chained stream async boom'),
+        findsOneWidget);
+  });
 }
