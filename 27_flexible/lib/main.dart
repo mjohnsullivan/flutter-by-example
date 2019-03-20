515e20e234a3e33abd6f25aa978b961223d14cd6
matt.j.sullivan@gmail.com <matt.j.sullivan@gmail.com>
2019-03-20 11:00:58 -0700

    Added Flexible example

---
+++ 27_flexible/lib/main.dart
@@ -0,0 +1,61 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flexible Widget Demo',
+      home: Scaffold(body: MyFlexiblePage()),
+    );
+  }
+}
+
+class MyFlexiblePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Container(color: Colors.yellow),
+        ),
+        Flexible(
+          flex: 2,
+          child: MyFlexibleRow(),
+        ),
+        Flexible(
+          flex: 3,
+          child: Container(color: Colors.blue),
+        ),
+      ],
+    );
+  }
+}
+
+class MyFlexibleRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex: 2,
+          child: Container(color: Colors.cyan),
+        ),
+        Flexible(
+          flex: 4,
+          child: Container(color: Colors.teal),
+        ),
+        Flexible(
+          flex: 2,
+          child: Container(color: Colors.indigo),
+        ),
+      ],
+    );
+  }
+}
