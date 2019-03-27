21566ea0186109152dcaa45c53884b3c9f538570
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-03-26 17:25:26 -0700

    Changed to new API and added engine preheat

--- 29_add_to_android_app/android/AddFlutterToApp/app/src/main/java/io/flutter/addfluttertoapp/MainActivity.kt
+++ 29_add_to_android_app/android/AddFlutterToApp/app/src/main/java/io/flutter/addfluttertoapp/MainActivity.kt
@@ -1,22 +1,24 @@
 package io.flutter.addfluttertoapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.NonNull
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.android.FlutterFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import io.flutter.facade.Flutter
-
 
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        removeAllFragments()
+        // removeAllFragments()
         when (item.itemId) {
             R.id.navigation_home -> {
                 addFragment(null, "I am an Android Fragment!!!")
@@ -27,7 +29,6 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                // addFragment(null, "I am a Flutter Fragment!!!")
                 addFlutterFragment()
                 return@OnNavigationItemSelectedListener true
             }
@@ -39,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         addFragment(null, "I am an Android Fragment!!!")
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
@@ -46,29 +48,19 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.fragment_container,
-                     AndroidFragment.newInstance(text),
-                    "mainFragment")
+                .replace(R.id.fragment_container,
+                    AndroidFragment.newInstance(text))
                 .commit()
-        }
-    }
-
-    private fun removeAllFragments() {
-        for (fragment in supportFragmentManager.fragments) {
-            supportFragmentManager.beginTransaction().remove(fragment).commit()
         }
     }
 
 
     private fun addFlutterFragment() {
         supportFragmentManager.beginTransaction()
-        .add(R.id.fragment_container,
-             Flutter.createFragment("flutterFragment"),
-             "flutterFragment")
-        .commit()
+            .replace(R.id.fragment_container,
+                     MyFlutterFragment.newInstance())
+            .commit()
     }
-
-
 }
 
 
@@ -93,5 +85,25 @@ class AndroidFragment : Fragment() {
             view.findViewById<TextView>(R.id.fragment_message).setText(it)
         }
         return view
+    }
+}
+
+class MyFlutterFragment : FlutterFragment() {
+
+    companion object {
+        fun newInstance(): FlutterFragment {
+            val fragment = MyFlutterFragment()
+            fragment.arguments = FlutterFragment.createArgsBundle(null, null, null, null)
+            return fragment
+        }
+    }
+
+    override fun onCreateFlutterEngine(@NonNull context: Context): FlutterEngine {
+        val app = context.applicationContext as MyApplication
+        return app.engine
+    }
+
+    override fun retainFlutterIsolateAfterFragmentDestruction() : Boolean {
+        return true
     }
 }
