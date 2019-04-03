770b314202fb27bf98ed00d77f38fecb3a6148cd
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-04-02 17:07:44 -0700

    Updated Align example

--- 25_align/lib/main.dart
+++ 25_align/lib/main.dart
@@ -1,14 +1,19 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Align Demo',
-      home: Scaffold(body: AlignExamples()),
+      home: Scaffold(
+        body: AlignExamples(),
+      ),
     );
   }
 }
@@ -16,55 +21,69 @@ class MyApp extends StatelessWidget {
 class AlignExamples extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Flexible(
-        flex: 1,
-        child: Align(
-          alignment: Alignment(0, 0),
-          child: Text('Centered!'),
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            color: Colors.black26,
+            child: Align(
+              alignment: Alignment(0, 0),
+              child: Text('Centered!'),
+            ),
+          ),
         ),
-      ),
-      Flexible(
-        flex: 1,
-        child: Stack(children: <Widget>[
-          Align(
-            alignment: Alignment(-1, -1),
-            child: Text('Top Left!'),
+        Flexible(
+          flex: 1,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black12,
+                ),
+              ),
+              Align(
+                alignment: Alignment(-1, -1),
+                child: Text('Top Left!'),
+              ),
+              Align(
+                alignment: Alignment(0, 0),
+                child: Text('Centered!'),
+              ),
+              Align(
+                alignment: Alignment(1, 1),
+                child: Text('Bottom Right!'),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text('TopCenter!'),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text('TopCenter!'),
+              ),
+            ],
           ),
-          Align(
-            alignment: Alignment(0, 0),
-            child: Text('Centered!'),
-          ),
-          Align(
-            alignment: Alignment(1, 1),
-            child: Text('Bottom Right!'),
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text('TopCenter!'),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text('TopCenter!'),
-          ),
-        ]),
-      ),
-      Flexible(
-        flex: 1,
-        child: Container(
-            child: Align(
-          alignment: Alignment(-.5, -.5),
-          child: Text('25% in'),
-        )),
-      ),
-      Flexible(
-        flex: 1,
-        child: Container(
-            child: Align(
-          alignment: Alignment(.5, .5),
-          child: Text('75% in'),
-        )),
-      ),
-    ]);
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+              color: Colors.black26,
+              child: Align(
+                alignment: Alignment(-.5, -.5),
+                child: Text('25% in'),
+              )),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+              color: Colors.black12,
+              child: Align(
+                alignment: Alignment(.5, .5),
+                child: Text('75% in'),
+              )),
+        ),
+      ],
+    );
   }
 }
