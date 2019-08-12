ddfe8a46e960d199eac3393d214fe28c59245964
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-08-12 14:43:08 -0700

    Updated RenderObject example

--- 34_render_object/lib/main.dart
+++ 34_render_object/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,25 +20,41 @@ void main() => runApp(
       ),
     );
 
+/// A typical way for creating layout widgets; use
+/// StatelessWidget and the framework layout widgets
+class BottomAsYouShouldWriteIt extends StatelessWidget {
+  BottomAsYouShouldWriteIt({this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => Align(
+        alignment: Alignment.bottomCenter,
+        child: child,
+      );
+}
+
+/// Dropping down a layer, we can create a new RenderObjectWidget
+/// to achieve the same effect. This is how Flutter has implemented
+/// the Center widget via the Align widget.
+///
 /// Here we're using a SingleChildRenderObject, which is
-/// appropriate for layout widgets wrapping a single child
+/// appropriate for layout widgets wrapping a single child.
+///
+/// A RenderPositionedBox is a RenderObject that lets us position
+/// RenderObjects within a Cartesian coordinate system. This
+/// RenderObject has an alignment property, where we can specify
+/// the position of its children.
 class Bottom extends SingleChildRenderObjectWidget {
   const Bottom({
     Key key,
     Widget child,
   }) : super(key: key, child: child);
 
+  ///
   @override
   RenderPositionedBox createRenderObject(BuildContext context) =>
       RenderPositionedBox(
         alignment: Alignment.bottomCenter,
-        widthFactor: 1,
-        heightFactor: 1,
         textDirection: Directionality.of(context),
       );
-
-  @override
-  void updateRenderObject(
-          BuildContext context, RenderPositionedBox renderObject) =>
-      renderObject.textDirection = Directionality.of(context);
 }
